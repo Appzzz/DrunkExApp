e15b6dc58c68da4da6d46d2ecf66e0a14a14f994
Appzzz <apptjj@gmail.com>
2013-07-15 11:56:33 +0200

    postavljamo a na nulu!

--- DrunkExApp/src/com/example/drunkexapp/Program.java
+++ DrunkExApp/src/com/example/drunkexapp/Program.java
@@ -17,6 +17,7 @@ public class Program extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.program, menu);
+        int a = 0;
         return true;
     }
     
